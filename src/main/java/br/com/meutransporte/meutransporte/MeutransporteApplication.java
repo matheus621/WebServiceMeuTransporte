2507b1ca8970cd93b1c0831265569afd63c487d3
matheus.souza <matheus621cardoso@gmail.com>
2018-12-03 21:11:27 -0200

    g

--- src/main/java/br/com/meutransporte/meutransporte/MeutransporteApplication.java
+++ src/main/java/br/com/meutransporte/meutransporte/MeutransporteApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MeutransporteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MeutransporteApplication.class, args);
+		SpringApplication.run(MeutransporteApplication.class, args); 
 	}
 }
